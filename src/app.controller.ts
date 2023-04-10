8e1991b77db3177c8455180c506be2f5b8d3aa8d
Abylay <abylay.madalkhan@gmail.com>
2023-04-10 14:44:31 +0600

    start

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateDto } from './dto/create.dto';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/:id')
+  getHello(@Param('id', ParseIntPipe) id: number): number {
+    if (id < 1) {
+      throw new BadRequestException('id must be greater than 0');
+    }
+
+    return id;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('create')
+  async create(@Body() dto: CreateDto) {
+    console.log('post create');
+    const res = await this.appService.save(dto);
+    return res;
   }
 }
